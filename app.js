462e0f40ebe38a914221bddb9b1df76592aa44bb
Gazizullin Malik <gazizullin.malik2010@yandex.ru>
2017-11-19 14:53:37 +0300

    Second TAsk

---
+++ app.js
@@ -0,0 +1,21 @@
+function onStartUp() {
+	
+	$("div#root").append("<ul><li><span>Сделать задание #3 по web-программированию</span><button>Удалить</button></li> </ul>");
+	bindToButton($("li button"));
+	$("div#root").append("<input type='text' id='add_task'/><button id='add_task'>Add Task</button>");
+	$("button#add_task").bind("click", function() {
+		$("ul").append("<li><span>" + $("input#add_task").val() +"</span><button>Удалить</button></li>");
+		bindToButton($("li button").last());
+		//bindToButton()
+	});	
+	console.log("WTF?");
+
+};
+
+
+function bindToButton(button) {
+	button.bind("click", function(event) {
+		button.parent().remove();
+	});
+};
+
